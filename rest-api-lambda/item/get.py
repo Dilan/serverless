55c9e9338125a54d5faad729e47e80adc39b3939
Plotnikov, Anton <anton.plotnikov@gmail.com>
2020-07-06 09:23:30 +0100

    REST API endpoints

--- rest-api-lambda/item/get.py
+++ rest-api-lambda/item/get.py
@@ -1,14 +1,38 @@
-import json
-import os
+import json, os, boto3
+
+bucket_name = os.environ['S3_BUCKET']
 
 def get(event, context):
 
-    id = event['pathParameters']['id']
+    id = int(event['pathParameters']['id'])
 
     # S3_BUCKET_STORAGE
     print(event)
 
-    item = { 'id': id }
+
+    resp = boto3.client('s3').list_objects_v2(Bucket=bucket_name)
+    counter = 0
+    for obj in resp['Contents']:
+
+        if obj['Size']:
+
+            if counter == id:
+                obj = boto3.resource('s3').Object(bucket_name=bucket_name, key=obj['Key'])
+                f = obj.get()['Body'].read()
+
+                return {
+                    "statusCode": 200,
+                    "headers": {
+                      'Content-Type': 'image/png'
+                    },
+                    "body": f,
+                    "isBase64Encoded": True
+                }
+
+
+            counter += 1
+
+
 
     return {
         "statusCode": 200,
--- rest-api-lambda/item/list.py
+++ rest-api-lambda/item/list.py
@@ -2,15 +2,25 @@ import json, os
 import boto3
 
 bucket_name = os.environ['S3_BUCKET']
-bucket = boto3.resource('s3').Bucket(bucket_name)
+
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
 
 def list(event, context):
 
     print("READ [" + bucket_name + "] sw3 bucket ... ")
     items = []
-    for obj in bucket.objects.all():
-        print(obj)
-        items.append(obj)
+    resp = boto3.client('s3').list_objects_v2(Bucket=bucket_name)
+    for obj in resp['Contents']:
+        if obj['Size']:
+            items.append({
+                'key': obj['Key'], 'size': sizeof_fmt(obj['Size'])
+            })
+            # items.append(obj['Key'] + ' :: ' + str(sizeof_fmt(obj['Size'])))
 
     return {
         "statusCode": 200,
